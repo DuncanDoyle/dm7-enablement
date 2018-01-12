2bdb350abd00a5403028a68c21b9b81e3c170112
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-01-12 12:17:05 +0100

    Added lab exercise. Moved original implementation to 'Solution'.

---
+++ dm7-rule-units-chaining-lab/src/main/java/com/redhat/dm7/demo/ruleunits/chained/units/OrchestrationUnitSolution.java
@@ -0,0 +1,23 @@
+package com.redhat.dm7.demo.ruleunits.chained.units;
+
+import org.kie.api.runtime.rule.DataSource;
+import org.kie.api.runtime.rule.RuleUnit;
+
+import com.redhat.dm7.demo.ruleunits.chained.model.Person;
+
+public class OrchestrationUnitSolution implements RuleUnit {
+
+	private DataSource<Person> persons;
+
+	public OrchestrationUnitSolution() {
+	}
+	
+	public DataSource<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(DataSource<Person> persons) {
+		this.persons = persons;
+	}
+	
+}
